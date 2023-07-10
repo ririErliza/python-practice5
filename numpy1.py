a2f2c1b37054cb7f4d34c1ee92ba70bd7cf110e1
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-10 09:10:59 +0200

    Update numpy1.py

---
+++ numpy1.py
@@ -0,0 +1,15 @@
+import numpy
+
+'''
+NumPy is used to work with arrays. 
+The array object in NumPy is called ndarray
+'''
+
+print(numpy.__version__)
+#1.25.0
+
+arr = numpy.array([1,2,3,4])
+print(arr)
+#[1 2 3 4]
+
+
