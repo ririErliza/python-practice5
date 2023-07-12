128b286a834f4e57e58e220a0715f8208d7e8c63
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-12 09:56:00 +0200

    Update numpy2.py

--- numpy2.py
+++ numpy2.py
@@ -30,3 +30,25 @@ print(x)
 #   [1. 1. 1.]
 #   [1. 1. 1.]]]
 
+
+''' RANGE '''
+
+x = np.arange(10)
+print(x)
+# [0 1 2 3 4 5 6 7 8 9]
+
+
+x = np.arange(5,10)
+print(x)
+# [5 6 7 8 9]
+
+
+x = np.arange(1,11,2) #step of 2
+print(x)
+# [1 3 5 7 9]
+
+
+x = np.arange(2,3, 0.1)
+print(x)
+# [2.  2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9]
+
