6c766228e8642545ed4f9e16b572fa44ee5f953a
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-13 11:38:58 +0200

    Update np_reorg_arr.py

--- np_reorg_arr.py
+++ np_reorg_arr.py
@@ -21,3 +21,27 @@ print(after)
 
 #  [[5 6]
 #   [7 8]]]
+
+
+''' Vertical Stack'''
+
+v1 = np.array([1,2,3])
+v2 = np.array([4,5,6])
+
+verA = np.vstack((v1,v2,v1,v2))
+print(verA)
+# [[1 2 3]
+#  [4 5 6]
+#  [1 2 3]
+#  [4 5 6]]
+
+
+''' Horizontal Stack'''
+
+h1 = np.ones((2,4))
+h2 = np.zeros((2,2))
+
+horA = np.hstack((h1,h2))
+print(horA)
+# [[1. 1. 1. 1. 0. 0.]
+#  [1. 1. 1. 1. 0. 0.]]
