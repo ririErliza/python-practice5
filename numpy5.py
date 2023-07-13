f6dd3045be7b232307b95a33d16a2bc2bdcfb68f
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-13 10:47:19 +0200

    Update numpy5.py

--- numpy5.py
+++ numpy5.py
@@ -6,9 +6,31 @@ import numpy as np
 
 a = np.ones((2,3))
 print(a)
+# [[1. 1. 1.]
+#  [1. 1. 1.]]
 
 b = np.full((3,2),2)
 print(b)
+# [[2 2]
+#  [2 2]
+#  [2 2]]
 
 z = np.matmul(a,b)
 print(z)
+# [[6. 6.]
+#  [6. 6.]]
+
+
+# Find the determinant
+c = np.identity(3)
+print(c)
+# [[1. 0. 0.]
+#  [0. 1. 0.]
+#  [0. 0. 1.]]
+
+y = np.linalg.det(c)
+print(y)
+# 1.0
+
+
+
